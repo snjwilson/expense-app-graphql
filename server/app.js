9005dafed8dd2b712d28d016c1c898c3f0a38543
Sanjay Wilson <sanjaywilsonktm@gmail.com>
2020-08-01 00:28:47 +0530

    [add] graphql and express-graphql

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({}));
 
 app.listen(5000, () => {
   console.log('Server is running!');
